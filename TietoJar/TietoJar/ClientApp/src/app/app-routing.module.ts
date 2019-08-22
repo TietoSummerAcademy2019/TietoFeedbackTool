d71cc0120ea7e16f1e31397dd1b889043b2b2ea5
Olejnik Wojciech <wojciech.olejnik@tieto.com>
2019-08-22 13:36:41 +0200

    Added basic form with style

--- TietoJar/TietoJar/ClientApp/src/app/app-routing.module.ts
+++ TietoJar/TietoJar/ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NewQuestionComponent } from './new-question/new-question.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'new-question',
+    component: NewQuestionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ TietoJar/TietoJar/ClientApp/src/app/new-question/new-question.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-question',
+  templateUrl: './new-question.component.html',
+  styleUrls: ['./new-question.component.scss']
+})
+export class NewQuestionComponent {
+
+  constructor() { }
+
+  onSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
+
+}
