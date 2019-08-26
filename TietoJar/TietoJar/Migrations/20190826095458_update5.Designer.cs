1ae2a5f16f2a44f1f2deddcf064bb9d521d500a1
Wozniak Patryk <patryk.wozniak@tieto.com>
2019-08-26 11:57:37 +0200

    Added to Context method to populate database with seed data

--- TietoJar/TietoJar/Migrations/20190826095458_update5.Designer.cs
+++ TietoJar/TietoJar/Migrations/20190826095458_update5.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TietoJar.Persistence;
 
 namespace TietoJar.Migrations
 {
     [DbContext(typeof(TietoJarContext))]
-    partial class TietoJarContextModelSnapshot : ModelSnapshot
+    [Migration("20190826095458_update5")]
+    partial class update5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
