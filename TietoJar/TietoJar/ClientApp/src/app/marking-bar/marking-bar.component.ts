84ab095ad2de07fc6e303b8a305cd0968f376bc0
Mateusz Grzybek <mateusz.grzybek@tieto.com>
2019-08-28 14:27:21 +0200

    Add bootstrap classes for collapse effect

--- TietoJar/TietoJar/ClientApp/src/app/marking-bar/marking-bar.component.ts
+++ TietoJar/TietoJar/ClientApp/src/app/marking-bar/marking-bar.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-marking-bar',
   templateUrl: './marking-bar.component.html',
-  styleUrls: ['./marking-bar.component.css']
+  styleUrls: ['./marking-bar.component.scss']
 })
 export class MarkingBarComponent implements OnInit {
 
+  public show:boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleState() {
+    this.show = !this.show;
+  }
 }
