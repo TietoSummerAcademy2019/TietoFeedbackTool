fb7a7aa3f4f8910a8114e0bc7751c349636f5657
Olejnik Wojciech <wojciech.olejnik@tieto.com>
2019-09-02 09:42:52 +0200

    Added table and paginator module

--- TietoJar/TietoJar/ClientApp/src/app/app.module.ts
+++ TietoJar/TietoJar/ClientApp/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { NewQuestionComponent } from './new-question/new-question.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DisplayDataComponent } from './display-data/display-data.component';
 import { MarkingBarComponent } from './marking-bar/marking-bar.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
    declarations: [
@@ -23,7 +25,9 @@ import { MarkingBarComponent } from './marking-bar/marking-bar.component';
       AppRoutingModule,
       BrowserAnimationsModule,
       FormsModule,
-      HttpClientModule
+      HttpClientModule,
+      MatTableModule,
+      MatPaginatorModule
    ],
    providers: [],
    bootstrap: [
