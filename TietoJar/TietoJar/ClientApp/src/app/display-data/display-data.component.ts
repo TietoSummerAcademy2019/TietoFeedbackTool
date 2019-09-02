0129ab1e3bb02f9595e5a8aa52bb5135ec6bcb88
Olejnik Wojciech <wojciech.olejnik@tieto.com>
2019-09-02 14:30:31 +0200

    Creating view data display

--- TietoJar/TietoJar/ClientApp/src/app/display-data/display-data.component.ts
+++ TietoJar/TietoJar/ClientApp/src/app/display-data/display-data.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DisplayDataService } from './display-data.service';
 import { Account } from '../models/Account';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
-
 
 @Component({
   selector: 'app-display-data',
@@ -13,17 +10,11 @@ import {MatTableDataSource} from '@angular/material/table';
 export class DisplayDataComponent implements OnInit {
 
   questionWithAnswer: Account[] = [];
-  displayedColumns: string[] = ['date', 'reaction', 'response', 'os', 'browser'];
-  dataSource = new MatTableDataSource<Account>(this.questionWithAnswer);
 
   constructor(private ds: DisplayDataService<Account>) { }
 
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-
   ngOnInit() {
     this.init();
-    console.log(this.dataSource);
-    this.dataSource.paginator = this.paginator;
   }
 
   async init() {
@@ -31,6 +22,7 @@ export class DisplayDataComponent implements OnInit {
     await this.ds.getAll().then((result) => {
       this.questionWithAnswer = result;
     });
-  }
+    console.log(this.questionWithAnswer);
 
+  }
 }
