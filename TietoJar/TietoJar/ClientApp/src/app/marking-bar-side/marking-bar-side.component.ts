58ecce0c148e755e5aadb2e605fbf125863e41c6
Mateusz Grzybek <mateusz.grzybek@tieto.com>
2019-09-04 11:53:50 +0200

    Replace angular animations with css transitions

--- TietoJar/TietoJar/ClientApp/src/app/marking-bar-side/marking-bar-side.component.ts
+++ TietoJar/TietoJar/ClientApp/src/app/marking-bar-side/marking-bar-side.component.ts
@@ -1,30 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, transition, style, animate } from '@angular/animations';
 import { MarkingBarService } from '../marking-bar/marking-bar.service';
 import { SurveyPuzzle } from '../models/SurveyPuzzle';
 import { OpenAnswer } from '../models/OpenAnswer';
 import { NgForm } from '@angular/forms';
-import { ButtonFadeAnimation } from '../animation';
 
 @Component({
   selector: 'app-marking-bar-side',
   templateUrl: './marking-bar-side.component.html',
-  styleUrls: ['./marking-bar-side.component.scss'],
-  animations: [
-    trigger('visibilityChanged', [
-      state('shown', style({ opacity: 1 })),
-      state('hidden', style({ opacity: 0 })),
-      transition('shown => hidden', animate('250ms')),
-      transition('hidden => shown', animate('250ms')),
-    ])
-  ]
+  styleUrls: ['./marking-bar-side.component.scss']
 })
 export class MarkingBarSideComponent implements OnInit {
-
-  public visibility: string = 'hidden';
   flag: boolean = false;
+  visibility: boolean = false;
   question: string = "";
-  public animation = new ButtonFadeAnimation;
 
   constructor(private mbs: MarkingBarService<SurveyPuzzle, OpenAnswer>) { };
 
@@ -46,6 +34,6 @@ export class MarkingBarSideComponent implements OnInit {
       this.question = this.mbs.getQuestion();
       this.flag = true;
     }
-    this.visibility = this.animation.toggle(this.visibility);
+    this.visibility = !this.visibility;
   }
 }
--- TietoJar/TietoJar/ClientApp/src/app/marking-bar/marking-bar.component.ts
+++ TietoJar/TietoJar/ClientApp/src/app/marking-bar/marking-bar.component.ts
@@ -3,28 +3,17 @@ import { MarkingBarService } from './marking-bar.service';
 import { SurveyPuzzle } from '../models/SurveyPuzzle';
 import { OpenAnswer } from '../models/OpenAnswer';
 import { NgForm } from '@angular/forms';
-import { trigger, state, transition, style, animate } from '@angular/animations';
-import { ButtonFadeAnimation } from '../animation';
 
 @Component({
   selector: 'app-marking-bar',
   templateUrl: './marking-bar.component.html',
-  styleUrls: ['./marking-bar.component.scss'],
-  animations: [
-    trigger('visibilityChanged', [
-      state('shown', style({ opacity: 1 })),
-      state('hidden', style({ opacity: 0 })),
-      transition('shown => hidden', animate('250ms')),
-      transition('hidden => shown', animate('250ms')),
-    ])
-  ]
+  styleUrls: ['./marking-bar.component.scss']
 })
 
 export class MarkingBarComponent implements OnInit {
-  public visibility: string = 'hidden';
   flag: boolean = false;
+  visibility: boolean = false;
   question: string = "";
-  public animation = new ButtonFadeAnimation;
 
   constructor(private mbs: MarkingBarService<SurveyPuzzle, OpenAnswer>) { };
 
@@ -41,7 +30,7 @@ export class MarkingBarComponent implements OnInit {
       this.question = this.mbs.getQuestion();
       this.flag = true;
     }
-    this.visibility = this.animation.toggle(this.visibility);
+    this.visibility = !this.visibility;
   }
 
   onSubmit(form: NgForm) {
