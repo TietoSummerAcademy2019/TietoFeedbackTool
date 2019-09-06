63aacad1f68283b97bfb9243c4d779ad52be02cb
Mateusz Grzybek <mateusz.grzybek@tieto.com>
2019-09-06 10:37:18 +0200

    Create service for fetching survey keys from the DB. Inject the service into the component

--- TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/tracking-code-generation/tracking-code-generation.component.ts
+++ TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/tracking-code-generation/tracking-code-generation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Survey } from '../models/Survey';
+import { TrackingCodeGenerationService } from './tracking-code-generation.service';
 
 @Component({
   selector: 'app-tracking-code-generation',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackingCodeGenerationComponent implements OnInit {
 
-  constructor() { }
+  survey: Survey;
+  surveyKey: string;
 
-  ngOnInit() { }
+  constructor(private tcs: TrackingCodeGenerationService<Survey>) { }
+
+  ngOnInit() {
+    this.init();
+  }
+
+  async init() {
+    await this.tcs.getSurveys().then((result) => {
+      this.survey = result;
+    });
+    this.surveyKey = this.survey[0].surveyKey;
+  }
 }
