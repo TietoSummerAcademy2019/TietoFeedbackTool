4df1634f9c1a0f5f79a0c1497b471b30feb6304b
Paweł Kubicki <kp39331@zut.edu.pl>
2019-09-10 11:02:11 +0200

    Changed localhost to server api

--- TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/environments/environment.ts
+++ TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/environments/environment.ts
@@ -3,10 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  newQuestionUrl: 'https://localhost:44350/api/survey/questions',
-  openAnswerUrl: 'https://localhost:44350/api/answer/open',
-  dataByLoginUrl: 'https://localhost:44350/api/survey/surveywithq/Jon',
-  surveyUrl: 'https://localhost:44350/api/survey',
+  newQuestionUrl: 'http://10.33.0.54:8083/api/survey/questions',
+  openAnswerUrl: 'http://10.33.0.54:8083/api/answer/open',
+  dataByLoginUrl: 'http://10.33.0.54:8083/api/survey/surveywithq/Jon',
+  surveyUrl: 'http://10.33.0.54:8083/api/survey',
   production: false
 };
 
