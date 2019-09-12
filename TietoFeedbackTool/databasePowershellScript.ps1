7cb959351ac630016523fbedac1b7d67691588f9
TietoSummerAcademy2019 <TietoSummerAcademy2019@yandex.com>
2019-09-12 12:05:35 +0200

    Added script for database handling on environments

---
+++ TietoFeedbackTool/databasePowershellScript.ps1
@@ -0,0 +1,45 @@
+﻿[String]$dbname = "TietoFeedbackTool";
+ 
+# Open ADO.NET Connection with Windows authentification to local SQLSERVER.
+$con = New-Object Data.SqlClient.SqlConnection;
+$con.ConnectionString = "Data Source=.;Initial Catalog=master;Integrated Security=True;";
+$con.Open();
+ 
+# Select-Statement for AD group logins
+$sql = "SELECT name
+        FROM sys.databases
+        WHERE name = '$dbname';";
+ 
+# New command and reader.
+$cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
+$rd = $cmd.ExecuteReader();
+if ($rd.Read())
+{   
+    $rd.Close();
+    $rd.Dispose();
+    $sql = "alter database [$dbname] set single_user with rollback immediate"
+    $cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
+    $cmd.ExecuteNonQuery();
+    $sql = "DROP DATABASE [$dbname]";
+    $cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
+    $cmd.ExecuteNonQuery();
+}
+$rd.Close();
+$rd.Dispose();
+ 
+# Create the database.
+$sql = "CREATE DATABASE [$dbname];"
+$cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
+$cmd.ExecuteNonQuery()
+
+$scriptName = "\scripts.sql";
+$server = "(local)";
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path;
+$scriptFullPath = $ScriptDir + $scriptName;
+
+Invoke-sqlcmd -ServerInstance $server -Database $dbname -InputFile $scriptFullPath
+ 
+# Close & Clear all objects.
+$cmd.Dispose();
+$con.Close();
+$con.Dispose();
