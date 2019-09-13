d06ecb1dc753191c602704d38c378de40838951d
Olejnik Wojciech <wojciech.olejnik@tieto.com>
2019-09-13 11:56:53 +0200

    Added possibility to return to the home page with change of icon

--- TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/tracking-code-generation/tracking-code-generation.component.ts
+++ TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/tracking-code-generation/tracking-code-generation.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Survey } from '../models/Survey';
 import { TrackingCodeGenerationService } from './tracking-code-generation.service';
 import { ClipboardService } from 'ngx-clipboard';
-import { element } from 'protractor';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tracking-code-generation',
   templateUrl: './tracking-code-generation.component.html',
   styleUrls: ['./tracking-code-generation.component.scss']
 })
-export class TrackingCodeGenerationComponent implements OnInit {
+export class TrackingCodeGenerationComponent implements OnInit, OnDestroy {
   survey: Survey;
   surveyKey: string;
   userSideScript: string;
 
   constructor(private tcs: TrackingCodeGenerationService<Survey>,
-    private _cs: ClipboardService) { }
+    private _cs: ClipboardService, private router: Router) {
+      if (this.router.url === '/tracking-code-generation') {
+        document.getElementById('icon').src = '../../assets/img/back.svg';
+        document.getElementById('icon').classList.add('icon-back');
+        document.getElementById('icon').classList.remove('kangaroo-icon');
+      }
+
+    }
 
   ngOnInit() {
     this.init();
+  }
+
+  ngOnDestroy(): void {
+        document.getElementById('icon').src = '../../assets/img/ninjakangoogeometric.png';
+        document.getElementById('icon').classList.remove('icon-back');
+        document.getElementById('icon').classList.add('kangaroo-icon');
+
   }
 
   async init() {
