dbde40f6ddc5bc3e787b228b43ccf55f968658d5
Olejnik Wojciech <wojciech.olejnik@tieto.com>
2019-09-16 08:14:14 +0200

    Changed function to change icon on navbar

--- TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/tracking-code-generation/tracking-code-generation.component.ts
+++ TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/tracking-code-generation/tracking-code-generation.component.ts
@@ -1,38 +1,23 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Account } from '../models/Account';
 import { TrackingCodeGenerationService } from './tracking-code-generation.service';
 import { ClipboardService } from 'ngx-clipboard';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tracking-code-generation',
   templateUrl: './tracking-code-generation.component.html',
   styleUrls: ['./tracking-code-generation.component.scss']
 })
-export class TrackingCodeGenerationComponent implements OnInit, OnDestroy {
+export class TrackingCodeGenerationComponent implements OnInit {
   acc: Account;
   questionsKey: string;
   userSideScript: string;
 
   constructor(private tcs: TrackingCodeGenerationService<Account>,
-    private _cs: ClipboardService, private router: Router) {
-      if (this.router.url === '/tracking-code-generation') {
-        document.getElementById('icon').src = '../../assets/img/back.svg';
-        document.getElementById('icon').classList.add('icon-back');
-        document.getElementById('icon').classList.remove('kangaroo-icon');
-      }
-
-    }
+    private _cs: ClipboardService) { }
 
   ngOnInit() {
     this.init();
-  }
-
-  ngOnDestroy(): void {
-        document.getElementById('icon').src = '../../assets/img/ninjakangoogeometric.png';
-        document.getElementById('icon').classList.remove('icon-back');
-        document.getElementById('icon').classList.add('kangaroo-icon');
-
   }
 
   async init() {
