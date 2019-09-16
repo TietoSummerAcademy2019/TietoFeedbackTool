fe6d8a38d0f5263284a3e8e9378acf3f36f3c1be
Zbytniewski Adam <adam.zbytniewski@tieto.com>
2019-09-16 13:05:42 +0200

    Fixed answer, added question required

--- TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/new-question/new-question.component.ts
+++ TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/new-question/new-question.component.ts
@@ -22,8 +22,23 @@ export class NewQuestionComponent {
 
   onSubmit(f: NgForm) {
     // get new-question from the form and assign it to the model
-    this.questionModel.questionText = f.controls['new-question'].value;
-    f.reset();
-    this.qs.add(this.questionModel);
+    let div = document.getElementById('question-area');
+
+    if (this.isEmptyOrSpaces(f.controls['new-question'].value)) {
+      div.style.backgroundColor = '#ffedf1';
+      div.style.borderColor = '#d9135d';
+      document.getElementById('need').style.display = 'inline';
+    } else {
+      this.questionModel.questionText = f.controls['new-question'].value;
+      f.reset();
+      this.qs.add(this.questionModel);
+      div.style.backgroundColor = 'white';
+      div.style.borderColor = '';
+      document.getElementById('need').style.display = 'none';
+    }    
+  }
+
+  isEmptyOrSpaces(str) {
+    return str === null || str.match(/^ *$/) !== null;
   }
 }
--- TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/assets/scripts/userSideScript.js
+++ TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/assets/scripts/userSideScript.js
@@ -83,8 +83,8 @@ function changeOpacity() {
 
 function checkAnswer() {
   if (isEmptyOrSpaces(document.getElementById('answer').value)) {
-    document.getElementById('answer').style.backgroundColor = "pink";
-    document.getElementById('answer').style.borderColor = "red";
+    document.getElementById('answer').style.backgroundColor =  "#ffedf1";
+    document.getElementById('answer').style.borderColor = "#d9135d";
     document.getElementById('need').style.display = 'inline';
   } else {
     changeHtmlContent();
