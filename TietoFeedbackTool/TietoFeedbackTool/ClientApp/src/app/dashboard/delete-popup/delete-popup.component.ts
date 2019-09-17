355f7cdee504dfe1cdc89fe06248c5b6bdc8fc06
Mateusz Grzybek <mateusz.grzybek@tieto.com>
2019-09-17 11:20:39 +0200

    Add possibility to close the popup window when clicking on cancel button

--- TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/dashboard/delete-popup/delete-popup.component.ts
+++ TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/dashboard/delete-popup/delete-popup.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Inject } from '@angular/core';
 import { DisplayDataService } from 'src/app/display-data/display-data.service';
 import { Account } from '../../models/Account';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 
 @Component({
@@ -13,11 +13,16 @@ export class DeletePopupComponent {
 
   constructor(
     private ds: DisplayDataService<Account>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<DeletePopupComponent>
   ) { }
 
   removeQuestion() {
     console.log(this.data.idQ);
     this.ds.remove(this.data.idQ)
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
   }
 }
