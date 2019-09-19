402b41848e2650a11cb80d425f5b1478628c4488
Mateusz Grzybek <mateusz.grzybek@tieto.com>
2019-09-19 15:12:59 +0200

    Remove new-page component

--- TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/app.module.ts
+++ TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/app/app.module.ts
@@ -18,7 +18,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { DeletePopupComponent } from './dashboard/delete-popup/delete-popup.component';
 import { MatDialogModule } from '@angular/material';
-import { NewPageComponent } from './new-page/new-page.component';
 
 export function setupTranslateFactory(
   service: TranslateService): Function {
@@ -36,7 +35,6 @@ export function setupTranslateFactory(
       TrackingCodeGenerationComponent,
       DashboardComponent,
       DeletePopupComponent,
-      NewPageComponent,
       TranslatePipe
    ],
    entryComponents: [
