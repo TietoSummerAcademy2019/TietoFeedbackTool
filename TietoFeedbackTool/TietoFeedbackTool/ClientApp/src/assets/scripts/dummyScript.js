f8d6ca9c9401d276a2f34648fc0f49e84567c740
Sebastian Dyjeta <sebastian.dyjeta@tieto.com>
2019-09-23 12:59:35 +0200

    dummyScript changed

--- TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/assets/scripts/dummyScript.js
+++ TietoFeedbackTool/TietoFeedbackTool/ClientApp/src/assets/scripts/dummyScript.js
@@ -1,11 +1,48 @@
 function surveySetup() {
+  var survey;
+  if (document.getElementsByName("new-answer").length == 0) {
+    survey = {
+      answer: document.getElementById('answer'),
+      questionId: document.getElementById('answer').getAttribute("data-id"),
+      submit: document.getElementById('survey-submit')
+    };
+  } else {
+    survey = {
+      questionId: document.getElementById('answer').getAttribute("data-id"),
+      submit: document.getElementById('survey-submit')
+    };
+  }
+
   survey.submit.addEventListener('click', () => {
+    var request = new XMLHttpRequest();
+
+    var requestData;
+    if (document.getElementsByName("new-answer").length == 0) {
+      requestData = {
+        answer: `${survey.answer.value}`,
+        questionId: `${survey.questionId}`
+      };
+    } else {
+      if (document.querySelector('input[name="new-answer"]:checked') != null) {
+        requestData = {
+          rating: `${document.querySelector('input[name="new-answer"]:checked').value}`,
+          questionId: `${survey.questionId}`
+        };
+      }
+    }
+
+    var jsonData = JSON.stringify(requestData);
+
+    var link = 'https://localhost:44350/api/Answer/open';
+    request.open('post', link);
+    request.setRequestHeader('Content-type', 'application/json');
+
+    request.send(jsonData);
     checkAnswer();
   });
 }
 
-function addCSS(isBottom)
-{
+function addCSS(isBottom) {
   var linkNode = document.createElement("link");
   linkNode.setAttribute("rel", "stylesheet");
   linkNode.setAttribute("type", "text/css");
@@ -13,7 +50,7 @@ function addCSS(isBottom)
   document.head.appendChild(linkNode);
 }
 function checkDomain() {
-  var apiLink = "https://localhost:44350/api/survey/getdummysurvey";
+  var apiLink = "https://localhost:44350/api/survey/getsurvey/" + key + "/" + currentDomain;
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -33,7 +70,7 @@ function changeHtmlContent() {
   document.getElementById('success-message').style.display = 'inline';
   setTimeout(function () {
     closeTool();
-   },1000); //delay is in milliseconds 
+  }, 1000); //delay is in milliseconds 
 }
 
 function closeTool() {
@@ -57,7 +94,7 @@ function changeOpacity() {
 
 function checkAnswer() {
   if (isEmptyOrSpaces(document.getElementById('answer').value)) {
-    document.getElementById('answer').style.backgroundColor =  "#ffedf1";
+    document.getElementById('answer').style.backgroundColor = "#ffedf1";
     document.getElementById('answer').style.borderColor = "#d9135d";
     document.getElementById('need').style.display = 'inline';
   } else {
@@ -66,5 +103,13 @@ function checkAnswer() {
 }
 
 function isEmptyOrSpaces(str) {
-  return str === null || str.match(/^\s* *$/) !== null;
+  if (document.getElementsByName("new-answer").length == 0) {
+    return str === null || str.match(/^\s* *$/) !== null;
+  } else {
+    if (document.querySelector('input[name="new-answer"]:checked') == null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
